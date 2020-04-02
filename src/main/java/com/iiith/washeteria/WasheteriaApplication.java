9c97a60b9aed6f4beffa9dc9f7cb522bddc77047
neerajBarthwal <neeraj.barthwal@gmail.com>
2020-04-03 01:26:39 +0530

    intial setup for backend

---
+++ src/main/java/com/iiith/washeteria/WasheteriaApplication.java
@@ -0,0 +1,14 @@
+package com.iiith.washeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WasheteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasheteriaApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/com/iiith/washeteria/service/TopicService.java
@@ -0,0 +1,25 @@
+package com.iiith.washeteria.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iiith.washeteria.dao.TopicDAO;
+import com.iiith.washeteria.dataentities.Topic;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicDAO topicDAO;
+	
+	public void addTopic(Topic topic) {
+		topicDAO.add(topic);
+		
+	}
+
+	public Topic getTopic(String id) {
+		// TODO Auto-generated method stub
+		return topicDAO.getById(id);
+	}
+
+}
